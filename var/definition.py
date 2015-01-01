ab578e40343377a09a823f80443beeee8fdf09dc
childe <heychilde@gmail.com>
2015-01-01 18:22:12 +0800

    new file:   definition.py new file:   num.py

---
+++ var/definition.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+i = 100 # 整数
+f = 3.14 # 浮点数
+s = "this is a string" # 字符串
+b = True # 布尔
+n = None # None
+print i
+print f
+print s
+print b
+print n
---
+++ var/num.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+a = 5
+b = 6
+
+print a, b
+
+a = a+1
+print a  # 6
+
+a += 2
+print a  # 8
+
+print a**2  # 64
+print a  # 8
+
+a **= 2
+print a  # 64
+
+print a/b  # 10. notice!
+print a%b  # 4
+print divmod(a, b)  # 10 4
