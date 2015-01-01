c67b80a2e8344fc38c261688ba56367adc0f2240
childe <heychilde@gmail.com>
2015-01-01 19:02:50 +0800

    add float modified:   var/num.py

--- var/num.py
+++ var/num.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# 整数
 a = 5
 b = 6
 
@@ -19,5 +20,17 @@ a **= 2
 print a  # 64
 
 print a/b  # 10. notice!
-print a%b  # 4
+print a % b  # 4
 print divmod(a, b)  # 10 4
+
+# 浮点数
+f = 100.123
+pi = 3.14
+print f/pi  # 31.8863057325
+print f//pi  # 31.0
+
+# 自动向高精度靠齐
+print type(a/b)  # <type 'int'>
+print type(a+b)  # <type 'int'>
+print type(a/f)  # <type 'float'>
+print type(a+f)  # <type 'float'>
