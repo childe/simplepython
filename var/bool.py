d06637588691353dbb7ca4f3e34ba05ec895983b
childe <heychilde@gmail.com>
2015-01-01 22:26:56 +0800

    new file:   bool.py

---
+++ var/bool.py
@@ -0,0 +1,19 @@
+if 1 < 2:
+    print True  # here
+else:
+    print False
+
+if "ABCD1234".isalnum():
+    print True  # here
+else:
+    print False
+
+if []:
+    print True
+else:
+    print False  # here
+
+if "":
+    print True
+else:
+    print False  # here
